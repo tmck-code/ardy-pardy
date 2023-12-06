8ed519f077aa2986bb839cfabac2d4997371a6d5
Tom McKeesick <tmck01@gmail.com>
2023-12-06 20:52:39 +1100

    add more colours

--- projects/xmas/xmas.ino
+++ projects/xmas/xmas.ino
@@ -13,17 +13,23 @@ int PIXEL_OFFSET = 0;
 #define YELLOW 1
 #define GREEN  2
 #define RED    3
+#define PINK   4
+#define CYAN   5
+#define MAGENTA 6
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // Green, Yellow
+// colours are Green, Red, Blue
 uint32_t COLOURS[] = {
   strip.Color(255, 255, 255),
   strip.Color(255, 255, 0),
   strip.Color(0,   255, 0),
-  strip.Color(255,   0, 0)
+  strip.Color(255,   0, 0),
+  strip.Color(0, 255, 255),
+  strip.Color(255, 0, 255),
+  strip.Color(0, 255, 100),
 };
-
 int N_COLOURS = sizeof(COLOURS)/sizeof(COLOURS[0]);
 uint32_t XMAS_DUO[2] = {RED, GREEN};
 uint32_t XMAS_TRIO[3] = {RED, GREEN, YELLOW};
@@ -35,16 +41,20 @@ void setup() {
   strip.setBrightness(50);
   strip.show();
 
-  Serial.println("Starting XMAS display with " + String(N_COLOURS) + " colours:");
+  Serial.println("Starting XMAS display with " + String(N_COLOURS) + " colours");
 }
 
 void loop() {
-  for (int i=0; i<N_COLOURS-1; i++) {
+  for (int i=0; i<N_COLOURS; i++) {
     colorWipe(COLOURS[i], SLEEP);
-    if (i % 2 == 0) {
-      xmas(XMAS_DUO, sizeof(XMAS_DUO)/sizeof(XMAS_DUO[0]), YELLOW);
-    } else {
-      xmas(XMAS_TRIO, sizeof(XMAS_TRIO)/sizeof(XMAS_TRIO[0]), WHITE);
+    for (int j=1; j<N_COLOURS; j++) {
+      if (i % 2 == 0) {
+        uint32_t colours_duo[2] = {i, (i+j)%(sizeof(COLOURS)/sizeof(COLOURS[0]))};
+        xmas(colours_duo, sizeof(colours_duo)/sizeof(colours_duo[0]), YELLOW);
+      } else {
+        uint32_t colours_trio[3] = {i, (i+1)%(sizeof(COLOURS)/sizeof(COLOURS[0])), (i+j)%sizeof(COLOURS)/sizeof(COLOURS[0])};
+        xmas(colours_trio, sizeof(XMAS_TRIO)/sizeof(XMAS_TRIO[0]), WHITE);
+      }
     }
   }
 }
