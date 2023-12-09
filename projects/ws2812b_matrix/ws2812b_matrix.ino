46c8433937f12aa9c60046eaa171e9e3ff6fc16f
Tom McKeesick <tmck01@gmail.com>
2023-12-09 13:51:20 +1100

    update

--- projects/ws2812b_matrix/ws2812b_matrix.ino
+++ projects/ws2812b_matrix/ws2812b_matrix.ino
@@ -5,7 +5,7 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 6
+#define PIN 4
 #define WIDTH 16
 #define HEIGHT 16
 char *choice;
@@ -20,11 +20,76 @@ const uint16_t colors[] = {
   matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
 };
 
-void setup() {
-  Serial.begin(9600);
-  matrix.begin();
-  matrix.setTextWrap(true);
-  matrix.setBrightness(5);
+void setPixelByIndex(int idx, int r, int g, int b) {
+  int curr = 0;
+  for (int y = 0; y<HEIGHT; y++) {
+    for (int x = 0; x<WIDTH; x++) {
+      if (curr == idx) {
+        matrix.drawPixel(x, y, matrix.Color(r, g, b));
+        return;
+      }
+      curr++;
+    }
+  }
+}
+
+void makeColorGradient(float frequency1, float frequency2, float frequency3,
+                            float phase1, float phase2, float phase3,
+                            float center, int width, int len)
+{
+  if (center == 0)   center = 128;
+  if (width == 0)    width = 127;
+  if (len == 0)      len = 50;
+
+  for (int i = 0; i < len; ++i)
+  {
+      int r = sin(frequency1*i + phase1) * width + center;
+      int g = sin(frequency2*i + phase2) * width + center;
+      int b = sin(frequency3*i + phase3) * width + center;
+      Serial.println(String(r)+","+String(g)+","+String(b));
+      // setColour(r,g,b);
+      setPixelByIndex(i, r, g, b);
+
+      if (i % 2 == 0) {
+        matrix.show();
+      }
+  }
+  matrix.show();
+}
+
+void setColour(int r, int g, int b) {
+  for (int y = 0; y<HEIGHT; y++) {
+    for (int x = 0; x<WIDTH; x++) {
+      matrix.drawPixel(x, y, matrix.Color(r, g, b));
+    }
+  }
+  matrix.show();
+}
+int r = 0;
+int g = 0;
+int b = 0;
+
+int current = 0;
+String line = "";
+
+void readRGB() {
+  if (Serial.available() > 0) {
+    line = Serial.readString();
+    if (current == 0) {
+      r = line.toInt();
+      Serial.println("r:"+line);
+    }
+    if (current == 1) {
+      g = line.toInt();
+      Serial.println("g:"+line);
+    }
+    if (current == 2) {
+      b = line.toInt();
+      Serial.println("rgb:"+String(r)+","+String(g)+","+String(b));
+      setColour(r, g, b);
+    }
+    current = (current+1)%3;
+  }
 }
 
 void horizontal(int r, int g, int b, int delay_ms = 20) {
@@ -58,14 +123,26 @@ void strip(int r, int g, int b, int xs, int width) {
   matrix.show();
 }
 
+int SLEEP = 3000;
+
+void setup() {
+  Serial.begin(9600);
+  matrix.begin();
+  matrix.setTextWrap(true);
+  matrix.setBrightness(5);
+}
 
 void loop() {
-  for (int r = 110; r<255; r+=10) {
-    for (int g = 110; g<255; g+=10) {
-      for (int b = 110; b<255; b+=10) {
-        strip(r, g, b, 3, 2);
-        delay(200);
-      }
-    }
-  }
+  // readRGB();
+  setColour(100, 0, 0);
+  makeColorGradient(2.3,.3,.3,0,2,4,192,64,256);
+  delay(SLEEP);
+
+  setColour(100, 0, 0);
+  makeColorGradient(.1,.2,.3,0,2,4,192,64,256);
+  delay(SLEEP);
+
+  setColour(100, 0, 0);
+  makeColorGradient(1.1,.2,.1,0,2,4,192,64,256);
+  delay(SLEEP);
 }
