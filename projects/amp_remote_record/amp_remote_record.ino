8cec0dd7628df6e5bd708e0d5cd0eff7f92e1184
Tom McKeesick <tmck01@gmail.com>
2024-08-29 21:52:19 +1000

    update
    // The "energy saving" LG remote signal
    // 4
    // 95
    // NEC
    // 6A95FB04
    // rawData[68]:
    //  -3276750
    //  +8950,-4450
    //  + 550,- 500 + 600,- 500 + 550,-1650 + 600,- 500
    //  + 550,- 550 + 550,- 550 + 550,- 550 + 550,- 550
    //  + 550,-1650 + 600,-1600 + 600,- 500 + 600,-1600
    //  + 600,-1650 + 600,-1650 + 550,-1650 + 550,-1650
    //  + 550,-1650 + 550,- 550 + 550,-1650 + 550,- 550
    //  + 550,-1650 + 600,- 500 + 600,- 550 + 550,-1650
    //  + 550,- 550 + 550,-1650 + 550,- 550 + 550,-1650
    //  + 550,- 550 + 550,-1650 + 550,-1650 + 550,- 550
    //  + 550
    // Sum: 66850
    // Protocol=NEC Address=0x4 Command=0x95 Raw-Data=0x6A95FB04 32 bits LSB
    
    // The "back" LG remote signal
    // 4
    // 1A
    // NEC
    // E51AFB04
    // rawData[68]:
    //  -3276750
    //  +8900,-4450
    //  + 550,- 550 + 550,- 550 + 550,-1600 + 600,- 550
    //  + 550,- 500 + 600,- 500 + 550,- 550 + 550,- 550
    //  + 550,-1650 + 550,-1650 + 550,- 550 + 550,-1650
    //  + 550,-1650 + 550,-1650 + 550,-1650 + 600,-1600
    //  + 600,- 500 + 550,-1650 + 550,- 550 + 550,-1650
    //  + 550,-1650 + 550,- 550 + 550,- 550 + 550,- 550
    //  + 550,-1650 + 550,- 550 + 550,-1650 + 550,- 500
    //  + 600,- 500 + 550,-1650 + 550,-1650 + 550,-1650
    //  + 600
    // Sum: 66650
    // Protocol=NEC Address=0x4 Command=0x1A Raw-Data=0xE51AFB04 32 bits LSB
    
    // The "down" LG remote signal
    // 4
    // 41
    // NEC
    // BE41FB04
    // rawData[68]:
    //  -3276750
    //  +8950,-4450
    //  + 550,- 550 + 550,- 550 + 500,-1700 + 550,- 550
    //  + 550,- 550 + 550,- 550 + 550,- 550 + 500,- 600
    //  + 500,-1700 + 550,-1650 + 550,- 550 + 550,-1650
    //  + 550,-1650 + 550,-1650 + 550,-1700 + 550,-1650
    //  + 550,-1650 + 550,- 600 + 550,- 550 + 550,- 550
    //  + 550,- 550 + 600,- 550 + 500,-1700 + 550,- 550
    //  + 600,- 550 + 500,-1700 + 550,-1650 + 550,-1700
    //  + 550,-1650 + 600,-1650 + 550,- 550 + 550,-1650
    //  + 600
    // Sum: 67100
    // Protocol=NEC Address=0x4 Command=0x41 Raw-Data=0xBE41FB04 32 bits LSB
    
    // The "up" LG remote signal
    // 4
    // 40
    // NEC
    // BF40FB04
    // rawData[68]:
    //  -3276750
    //  +8900,-4450
    //  + 550,- 550 + 550,- 550 + 500,-1700 + 550,- 550
    //  + 550,- 550 + 550,- 550 + 550,- 550 + 550,- 550
    //  + 500,-1700 + 500,-1700 + 550,- 550 + 500,-1700
    //  + 500,-1700 + 500,-1700 + 550,-1650 + 550,-1650
    //  + 550,- 550 + 500,- 600 + 500,- 600 + 500,- 550
    //  + 550,- 550 + 550,- 550 + 550,-1650 + 550,- 550
    //  + 550,-1650 + 550,-1650 + 550,-1650 + 550,-1650
    //  + 550,-1650 + 550,-1650 + 550,- 550 + 550,-1650
    //  + 550
    // Sum: 66650
    // Protocol=NEC Address=0x4 Command=0x40 Raw-Data=0xBF40FB04 32 bits LSB

--- projects/amp_remote_record/amp_remote_record.ino
+++ projects/amp_remote_record/amp_remote_record.ino
@@ -1,25 +1,104 @@
 #include<IRremote.hpp>
 
 #define IR_RECEIVE_PIN 11
+#define IR_SEND_PIN 3
+
+#define NRG_AUTO 0
+#define NRG_OFF 1
+#define NRG_MIN 2
+#define NRG_MED 3
+#define NRG_MAX 4
+#define NRG_SCREEN_OFF 5
+#define NRG_N_OPTIONS 6
+
+int energy_saving_options[6] = {NRG_AUTO, NRG_OFF, NRG_MIN, NRG_MED, NRG_MAX, NRG_SCREEN_OFF};
+int current_energy_saving_option = -1;
 
 void setup() {
     Serial.begin(9600);
     Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
     // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
     IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+    //  IrSender.begin(IR_SEND_PIN);
+    IrSender.begin(IR_SEND_PIN, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN); // Specify send pin and enable feedback LED at default feedback LED pin
+
     Serial.println("Ready to receive unknown IR signals at pin " + String(IR_RECEIVE_PIN) + " and decode it with hash decoder.");
+
+    Serial.print(F("Send IR signals at pin "));
+    Serial.println(IR_SEND_PIN);
+    delay(5000);
+
+    Serial.println("Init energy saving");
+    initEnergySaving();
+}
+
+#define IR_UP 0x40
+#define IR_DOWN 0x41
+#define IR_BACK 0x1A
+#define IR_OPEN_ENERGY_SAVING 0x95
+
+void selectEnergySavingOption(int option) {
+  if (option < 0 || option >= NRG_N_OPTIONS) {
+    Serial.println("Invalid energy saving option: " + String(option));
+    return;
+  } else if (current_energy_saving_option == option) {
+    Serial.println("Already selected energy saving option: " + String(option));
+    return;
+  } else if (current_energy_saving_option < option) {
+    for (int i = current_energy_saving_option; i < option; i++) {
+      Serial.println("pressing down, i: " + String(i));
+      IrSender.sendNEC(0x4, IR_DOWN, 0);
+      delay(500);
+    }
+  } else {
+    for (int i = current_energy_saving_option; i > option; i--) {
+      Serial.println("pressing up, i: " + String(i));
+      IrSender.sendNEC(0x4, IR_UP, 0);
+      delay(500);
+    }
+  }
+}
+
+void initEnergySaving() {
+  Serial.println("opening energy saving options");
+  IrSender.sendNEC(0x4, IR_OPEN_ENERGY_SAVING, 0);
+  delay(500);
+  // cycle up to the first option and beyond, to the "close window" button.
+  // ensure that the "close window" button is definitely selected.
+  // 1. get the length of the energy_saving_options array
+  for (int i = 0; i < NRG_N_OPTIONS+1; i++) {
+    Serial.println("pressing up, i: " + String(i));
+    IrSender.sendNEC(0x4, IR_UP, 0);
+    delay(500);
+  }
+  // at this point, the menu index is _actually_ at -1!
+  // let's select "off", i.e. turn the energy saving off (max brightness)
+  selectEnergySavingOption(NRG_OFF);
+  // woohoo! let's wait 5 seconds, then select max energy saving
+  delay(5000);
+  selectEnergySavingOption(NRG_MAX);
+}
+
+void printIRSignal() {
+  IrReceiver.initDecodedIRData(); // is required, if we do not call decode();
+  IrReceiver.decode();
+  // IrReceiver.decodeHash();
+  Serial.println(IrReceiver.decodedIRData.address, HEX);
+  Serial.println(IrReceiver.decodedIRData.command, HEX);
+  Serial.println(getProtocolString(IrReceiver.decodedIRData.protocol));
+  Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+
+  // We have an unknown protocol here, print extended info
+  IrReceiver.printIRResultRawFormatted(&Serial, true);
+  IrReceiver.printIRResultShort(&Serial);
 }
 
 void loop() {
   if (IrReceiver.available()) {
-    IrReceiver.initDecodedIRData(); // is required, if we do not call decode();
-    IrReceiver.decodeHash();
+    printIRSignal();
 
-    // We have an unknown protocol here, print extended info
-    IrReceiver.printIRResultRawFormatted(&Serial, true);
+    delay(1000);
     IrReceiver.resume(); // Early enable receiving of the next IR frame
-
-    IrReceiver.printIRResultShort(&Serial);
-    Serial.println();
   }
 }
+
