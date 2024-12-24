4e2e63d86d990cfd86f120bc76dfed3b26feeada
Tom McKeesick <tmck01@gmail.com>
2024-12-24 22:39:56 +1100

    update - draw xmas tree and move it

--- projects/ws2812b_matrix/ws2812b_matrix.ino
+++ projects/ws2812b_matrix/ws2812b_matrix.ino
@@ -21,26 +21,93 @@ const uint16_t colors[] = {
   matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
 };
 
+const char* xmas_tree[][16] = {
+  {" ", " ", " ", " ", " ", " ", " ", "Z", " ", " ", " ", " ", " ", " ", " ", " "},
+  {" ", " ", " ", " ", " ", " ", "Y", "X", "Y", " ", " ", " ", " ", " ", " ", " "},
+  {" ", " ", " ", " ", " ", "Z", "X", "X", "X", "Z", " ", " ", " ", " ", " ", " "},
+  {" ", " ", " ", " ", " ", "Y", "X", "X", "X", "X", " ", " ", " ", " ", " ", " "},
+  {" ", " ", " ", " ", "Z", "X", "Z", "X", "X", "X", "Z", " ", " ", " ", " ", " "},
+  {" ", " ", " ", " ", "X", "X", "X", "X", "X", "X", "X", " ", " ", " ", " ", " "},
+  {" ", " ", " ", "Z", "X", "X", "X", "X", "X", "X", "X", "Z", " ", " ", " ", " "},
+  {" ", " ", " ", "X", "X", "X", "X", "X", "Z", "Y", "X", "X", " ", " ", " ", " "},
+  {" ", " ", "Z", "X", "X", "Y", "X", "X", "X", "X", "X", "X", "Z", " ", " ", " "},
+  {" ", " ", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ", " ", " "},
+  {" ", "Z", "X", "X", "X", "Z", "X", "X", "X", "X", "X", "X", "X", "Z", " ", " "},
+  {" ", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ", " "},
+  {"Z", "X", "X", "X", "X", "Y", "X", "X", "X", "X", "Z", "X", "X", "X", "Z", " "},
+  {" ", " ", " ", " ", " ", " ", " ", "O", " ", " ", " ", " ", " ", " ", " ", " "},
+  {" ", " ", " ", " ", " ", " ", " ", "O", " ", " ", " ", " ", " ", " ", " ", " "},
+  {" ", " ", " ", " ", " ", " ", " ", "O", " ", " ", " ", " ", " ", " ", " ", " "},
+};
+
 void setup() {
   Serial.begin(9600);
   matrix.begin();
-  matrix.setTextWrap(true);
   matrix.setBrightness(5);
 }
 
-void loop() {
-  for (int r = 50; r<255; r+=10) {
-    for (int g = 50; g<255; g+=10) {
-      for (int b = 50; b<255; b+=10) {
-        serial_printf(Serial, "r: %d, g: %d, b: %d\n", r, g, b);
-        for (int y = 0; y<HEIGHT; y++) {
-          for (int x = 0; x<WIDTH; x++) {
-            matrix.drawPixel(x, y, matrix.Color(r, g, b));
-          }
-          matrix.show();
-          delay(20);
+void drawXmasTree(int x_offset, int y_offset, bool flash) {
+  for (int y = 0; y<HEIGHT; y++) {
+    for (int x = 0; x<WIDTH; x++) {
+      if (xmas_tree[y][x] == " ") {
+        matrix.drawPixel((x+x_offset)%16, (y+y_offset)%16, matrix.Color(0, 0, 0));
+      } else if (xmas_tree[y][x] == "X") {
+        matrix.drawPixel((x+x_offset)%16, (y+y_offset)%16, matrix.Color(0, 255, 0));
+      } else if (xmas_tree[y][x] == "O") {
+        matrix.drawPixel((x+x_offset)%16, (y+y_offset)%16, matrix.Color(255, 0, 0));
+      } else if (xmas_tree[y][x] == "Z") {
+        if (flash == true) {
+          matrix.drawPixel((x+x_offset)%16, (y+y_offset)%16, matrix.Color(255, 255, 0));
+        } else {
+          matrix.drawPixel((x+x_offset)%16, (y+y_offset)%16, matrix.Color(255, 0, 0));
+        }
+      } else if (xmas_tree[y][x] == "Y") {
+        if (flash == true) {
+          matrix.drawPixel((x+x_offset)%16, (y+y_offset)%16, matrix.Color(0, 255, 0));
+        } else {
+          matrix.drawPixel((x+x_offset)%16, (y+y_offset)%16, matrix.Color(0, 0, 255));
         }
       }
     }
   }
+  matrix.show();
+  delay(100);
+}
+
+void loop() {
+  for (int x = 0; x<=16; x++) {
+    drawXmasTree(x, 0, true);
+  }
+  bool flash = true;
+  for (int i=0; i<4; i++) {
+    drawXmasTree(0, 0, flash);
+    flash = !flash;
+    delay(300);
+  }
+  delay(600);
+  for (int y = 0; y<=16; y++) {
+    drawXmasTree(0, y, false);
+  }
+  flash = false;
+  for (int i = 0; i <4; i++) {
+    drawXmasTree(0, 0, flash);
+    flash = !flash;
+    delay(300);
+  }
+  delay(600);
+  for (int y = 0; y<=32; y++) {
+    if (y <= 16) {
+      drawXmasTree(y, y, true);
+    } else {
+      drawXmasTree(y, y, false);
+    }
+  }
+  delay(600);
+  flash = true;
+  for (int i = 0; i <3; i++) {
+    drawXmasTree(0, 0, flash);
+    flash = !flash;
+    delay(300);
+  }
+  delay(600);
 }
